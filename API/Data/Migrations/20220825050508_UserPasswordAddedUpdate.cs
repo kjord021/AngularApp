074b133c60b9903cf24677f4aa62bffe4184606b
kjord021 <kjord021@fiu.edu>
2022-08-25 03:02:40 -0400

    Added Password Authhentication and JWT

---
+++ API/Data/Migrations/20220825050508_UserPasswordAddedUpdate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    public partial class UserPasswordAddedUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "PasswordSalt",
+                table: "Users",
+                type: "BLOB",
+                nullable: true,
+                oldClrType: typeof(byte),
+                oldType: "INTEGER");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte>(
+                name: "PasswordSalt",
+                table: "Users",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: (byte)0,
+                oldClrType: typeof(byte[]),
+                oldType: "BLOB",
+                oldNullable: true);
+        }
+    }
+}
